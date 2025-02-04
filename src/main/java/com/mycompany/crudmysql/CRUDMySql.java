2d251805c2fd988be5a7bc53acbec9aeea70ef78
Jesus Omar Leal <j3sus0marle@gmail.com>
2025-02-03 22:38:39 -0800

    Initial commit

---
+++ src/main/java/com/mycompany/crudmysql/CRUDMySql.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudmysql;
+
+/**
+ *
+ * @author omar-
+ */
+public class CRUDMySql {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
